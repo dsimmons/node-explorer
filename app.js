2e363f81ccef1cad3e6b6a9ac5c5ee64088f29d1
Dan Simmons <xiode04@gmail.com>
2011-08-03 15:35:14 +0000

    Added Jade templating with default login screen, basic routing, basic directory structure, dependencies.

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 
-/**
- * Module dependencies.
- */
-
+// Module dependencies
 var express = require('express');
-
 var app = module.exports = express.createServer();
 
 // Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
+  app.use(express.session({ secret: 'explore-node' }));
   app.use(express.compiler({ src: __dirname + '/public', enable: ['sass'] }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
@@ -30,10 +25,9 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
+  res.render('login', {
+    title: 'Login'
   });
 });
 
